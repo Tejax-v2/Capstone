86dd32d41fb1be1ff0757c3ceca5a1028be41c25
Tejax-v2 <tejastupke74@gmail.com>
2024-04-24 05:54:56 +0530

    Safe spot

--- time_server.py
+++ time_server.py
@@ -8,6 +8,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
 import os
+from email.mime.base import MIMEBase
+from email import encoders
 
 # Load environment variables from .env file
 load_dotenv()
@@ -50,13 +52,37 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
             body = 'This is a test email sent from Python.'
             # Attach body to message
             msg.attach(MIMEText(body, 'plain'))
+
+            attachment_path = '/home/tejax/Desktop/Capstone/plots.pdf'
+
+            # Open and read the attachment in binary mode
+            with open(attachment_path, 'rb') as attachment:
+                # Add file as application/octet-stream
+                # Email client can usually download this automatically as attachment
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload(attachment.read())
+
+            # Encode file in ASCII characters to send by email    
+            encoders.encode_base64(part)
+
+            # Add header as key/value pair to attachment part
+            part.add_header(
+                'Content-Disposition',
+                f'attachment; filename= {attachment_path.split("/")[-1]}',
+            )
+
+            # Add attachment to message and convert message to string
+            msg.attach(part)
+            text = msg.as_string()
+
+
             # Establish a connection to the SMTP server
             server = smtplib.SMTP('smtp.office365.com', 587)  # Replace with your SMTP server and port
             server.starttls()
             # Login to the SMTP server
             server.login(sender_email, password)
             # Send the email
-            server.sendmail(sender_email, receiver_email, msg.as_string())
+            server.sendmail(sender_email, receiver_email, text)
             # Close the connection
             server.quit()
             print('Email sent successfully!')
